9a673c136595e83f77a2c27c035a19dadb9b7ac3
rodrigoemanuelde <rodrigoemanuelde@hotmail.com>
2020-03-16 12:38:39 -0300

    Implementación random
    implementación de las 4 imágenes random

---
+++ js/random.js
@@ -0,0 +1,40 @@
+const url = 'https://api.giphy.com/v1/gifs/random?';
+const apiKey = 'mmvn9BQ98KLdBeCFwNbuvaOUSu0MMeEx';
+
+fetch(url + "&api_key=" + apiKey)
+    .then(res => res.json())
+    .then(data => {
+        //console.log(data.data)
+        let element = document.getElementById('random')
+        random.innerHTML = `<img src="${data.data.fixed_width_downsampled_url}">`
+        //console.log(data.data.fixed_width_downsampled_url)
+
+    })
+    .catch(err => console.log(err))
+
+fetch(url + "&api_key=" + apiKey)
+    .then(res => res.json())
+    .then(data => {
+        //console.log(data.data.image_url)
+        let element = document.getElementById('random1')
+        random1.innerHTML = `<img src="${data.data.fixed_width_downsampled_url}">`
+    })
+    .catch(err => console.log(err))
+
+fetch(url + "&api_key=" + apiKey)
+    .then(res => res.json())
+    .then(data => {
+        //console.log(data.data.image_url)
+        let element = document.getElementById('random2')
+        random2.innerHTML = `<img src="${data.data.fixed_width_downsampled_url}">`
+    })
+    .catch(err => console.log(err))
+
+fetch(url + "&api_key=" + apiKey)
+    .then(res => res.json())
+    .then(data => {
+        //console.log(data.data.image_url)
+        let element = document.getElementById('random3')
+        random3.innerHTML = `<img src="${data.data.fixed_width_downsampled_url}">`
+    })
+    .catch(err => console.log(err))
--- js/tend.js
+++ js/tend.js
@@ -1,10 +1,11 @@
-let api = 'api_key=80bfcbf357864cd18518c324f47a7098';
-let url = 'https://api.giphy.com/v1/gifs/trending?';
+const api = 'api_key=mmvn9BQ98KLdBeCFwNbuvaOUSu0MMeEx';
+const url2 = 'https://api.giphy.com/v1/gifs/trending?';
 
 const gifsBox = document.querySelector('.tend');
 
 
-fetch(url + api)
+
+fetch(url2 + api)
     .then(response => response.json())
     .then(data => {
         data.data.forEach(function (gif) {
