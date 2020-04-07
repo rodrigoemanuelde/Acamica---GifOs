dbf4f0f1467a445b2c429b60a4a30e701e0535da
rodrigoemanuelde <rodrigoemanuelde@hotmail.com>
2020-04-07 10:35:02 -0300

    corrección link

--- js/page.js
+++ js/page.js
@@ -1,21 +1,20 @@
 //para cambiar de pagina (misGuifos)
 
-let misGuifos = document.getElementById("misGif");
-misGuifos.addEventListener("click", () => {
-    location.assign("/misgif.html");
+let misGuifos = document.getElementById('misGif');
+misGuifos.addEventListener('click', () => {
+  location.assign('..//misgif.html');
 });
 
 //para cambiar de página (Crear Gifo)
 
-let upload = document.getElementById("crear");
-upload.addEventListener("click", () => {
-    location.assign("//creargifo.html");
+let upload = document.getElementById('crear');
+upload.addEventListener('click', () => {
+  location.assign('..//creargifo.html');
 });
-
 
 //para cambiar de página (Inicio)
 
-let index = document.getElementById("logo");
-index.addEventListener("click", () => {
-    location.assign("//index.html");
+let index = document.getElementById('logo');
+index.addEventListener('click', () => {
+  location.assign('..//index.html');
 });
