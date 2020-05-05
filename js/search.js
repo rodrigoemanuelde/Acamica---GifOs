b87ca6bd11306b222fcb1072e4e93de0ed850175
rodrigoemanuelde <rodrigoemanuelde@hotmail.com>
2020-05-05 20:12:20 -0300

    Autocompletad
    Haciendo funcionar el autocompletado

--- js/search.js
+++ js/search.js
@@ -9,16 +9,16 @@ searchForm.addEventListener('submit', function (e) {
     search(q)
 });
 
-function search(q) {
+async function search(q) {
     const apikey = 'DmRJY1L77G6uZ6eloQQd0cXSfD6OsEXC'
     const path = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}`
 
     // Busqueda
     //https://api.giphy.com/v1/gifs/search?api_key=mmvn9BQ98KLdBeCFwNbuvaOUSu0MMeEx&q=cats
 
-    fetch(path).then((res) => res.json())
+    await fetch(path).then((res) => res.json())
         .then(function (json) {
-            console.log(json.data[0].images.fixed_width.url)
+            //console.log(json.data[0].images.fixed_width.url)
             let resultsHTML = ''
 
             json.data.forEach(function (obj) {
@@ -39,14 +39,37 @@ function search(q) {
             resultEl.innerHTML = resultsHTML
         }).catch(function (err) {
             console.log(err.message)
-        })
+        });
+
 }
 
+/* searchForm.addEventListener('keypress', function (e) {
+    e.preventDefault();
+    const input = searchInput.value;
 
-function autocompleteSearch() {
-    const found = fetch(`https://api.giphy.com/v1/gifs/search/tags?q=${earchInput}&api_key=${apikey}&limit=3`)
-        .then(data => data.json())
-        .then(content => content)
-        .catch(e => console.log(e))
-    return found;
-}
+    console.log(input)
+});
+ */
+
+
+
+
+
+const apikey = 'DmRJY1L77G6uZ6eloQQd0cXSfD6OsEXC';
+const input = searchInput.value;
+
+fetch(`https://api.giphy.com/v1/gifs/search/tags?q=${input}&api_key=${apikey}&limit=3`)
+    .then(response => response.json())
+    .then(data => {
+        //console.log(data.data[1].name);
+
+        sug1 = document.getElementById('sugerencias');
+        sug1.innerHTML += `<p id="sugerencia1">${data.data[0].name}</p>`;
+
+        sug2 = document.getElementById('sugerencias');
+        sug2.innerHTML += `<p id="sugerencia2">${data.data[1].name}</p>`;
+
+        sug3 = document.getElementById('sugerencias');
+        sug3.innerHTML += `<p id="sugerencia3">${data.data[2].name}</p>`;
+
+    })
