2f88b42461cb10fec9f1388aeb7cda75c91c8bf4
rodrigoemanuelde <rodrigoemanuelde@hotmail.com>
2020-05-10 12:56:30 -0300

    Inicio trabajo de cámara
    Se inició la implementación de la cámara y captura de gif

---
+++ js/capturarGif.js
@@ -0,0 +1,66 @@
+const image = document.querySelector('#video');
+const btnGrabar = document.getElementById('btn-start-recording');
+const btnParar = document.getElementById('btn-stop-recording');
+const btnComenzarPimero = document.getElementById('comenzarPrimero');
+const btnCancelarPrimero = document.getElementById('cancelarPrimero');
+
+
+function captureCamera(callback) {
+    navigator.mediaDevices
+        .getUserMedia({
+            video: true,
+        })
+        .then(function (camera) {
+            callback(camera);
+        })
+        .catch(function (error) {
+            alert(
+                'Unable to capture your camera. Please check console logs.'
+            );
+            console.error(error);
+        });
+}
+
+function stopRecordingCallback() {
+    image.src = URL.createObjectURL(recorder.getBlob());
+    recorder.camera.stop();
+    recorder.destroy();
+    recorder = null;
+}
+
+const recorder; // globally accessible
+
+btnGrabar.addEventListener('click', () => {
+    btnGrabar.disabled = true;
+    captureCamera(function (camera) {
+        document.querySelector('h1').innerHTML =
+            'Waiting for Gif Recorder to start...';
+        recorder = RecordRTC(camera, {
+            type: 'gif',
+            frameRate: 1,
+            quality: 10,
+            width: 360,
+            hidden: 240,
+            onGifRecordingStarted: () => {
+                document.querySelector('h1').innerHTML =
+                    'Gif recording started.';
+            },
+            onGifPreview: function (gifURL) {
+                image.src = gifURL;
+            },
+        });
+
+        recorder.startRecording();
+
+        // release camera on stopRecording
+        recorder.camera = camera;
+
+        btnParar.disabled = false;
+    });
+})
+
+
+btnParar.addEventListener('click', () => {
+    btnParar.disabled = true;
+    recorder.stopRecording(stopRecordingCallback);
+})
--- js/theme.js
+++ js/theme.js
@@ -1,11 +1,13 @@
 // Cambio de tema de la página - day & nigth
 
+
 const day = document.getElementById('day');
 const night = document.getElementById('night');
 const logo = document.getElementById('logo');
 const favicon = document.getElementById('favicon');
 const flecha = document.getElementById('arrow');
 
+//Evento click para el tema day
 day.addEventListener('click', () => {
   document.documentElement.setAttribute('dataTheme', 'light');
   logo.src = 'img/gifOF_logo.png';
@@ -15,6 +17,7 @@ day.addEventListener('click', () => {
   localStorage.setItem('dataTheme', 'day');
 });
 
+//Evento click para el tema night
 night.addEventListener('click', () => {
   document.documentElement.setAttribute('dataTheme', 'dark');
   logo.src = 'img/gifOF_logo_dark.png';
@@ -24,7 +27,7 @@ night.addEventListener('click', () => {
   localStorage.setItem('dataTheme', 'night');
 });
 
-// Comprobación modo actual
+// Comprobación modo actual y guardado en localstorage
 
 if (localStorage.getItem('dataTheme') === 'day') {
   document.documentElement.setAttribute('dataTheme', 'light');
